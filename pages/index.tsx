6830a866b4b67f74aad60f3aa59370ea589393ff
Jose <dreamzzediting@gmail.com>
2020-11-21 17:36:49 -0500

    Converted to Typescript + added tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+      <div>
+      <div className="py-20">
+        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
+          Next.js + Tailwind CSS 2.0
+        </h1>
+      </div>
+    </div>
+  )
+}
